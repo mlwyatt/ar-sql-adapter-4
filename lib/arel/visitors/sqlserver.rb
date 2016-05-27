84f1597c125252f79f433e5e7ec3ab143e00a97b
Marcus Wyatt <mlwyatt2008@gmail.com>
2016-05-27 15:21:37 -0400

    too many params

--- lib/arel/visitors/sqlserver.rb
+++ lib/arel/visitors/sqlserver.rb
@@ -133,7 +133,7 @@ module Arel
 
       # SQLServer ToSql/Visitor (Additions)
 
-      def visit_Arel_Nodes_SelectStatementWithOutOffset(o, windowed=false)
+      def visit_Arel_Nodes_SelectStatementWithOutOffset(o)
         find_and_fix_uncorrelated_joins_in_select_statement(o)
         core = o.cores.first
         projections = core.projections
@@ -174,7 +174,7 @@ module Arel
           (rowtable_projections(o).map{ |x| visit(x) }.join(', ')),
           "FROM (",
             "SELECT #{core.set_quantifier ? 'DISTINCT DENSE_RANK()' : 'ROW_NUMBER()'} OVER (ORDER BY #{orders.map{ |x| visit(x) }.join(', ')}) AS [__rn],",
-            visit_Arel_Nodes_SelectStatementWithOutOffset(o,true),
+            visit_Arel_Nodes_SelectStatementWithOutOffset(o),
           ") AS [__rnt]",
           (visit(o.offset) if o.offset),
           "ORDER BY [__rnt].[__rn] ASC"
